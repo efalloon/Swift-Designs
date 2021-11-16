bcdd3237867c10bf855f28a93f996e3757d22592
Euan Traynor <efalloon057@icloud.com>
2021-11-16 22:23:21 +1030

    update

--- Apps/Nike Shoe App/Nike Shoe App/view controllers/ShoeInspectionViewController.swift
+++ Apps/Nike Shoe App/Nike Shoe App/view controllers/ShoeInspectionViewController.swift
@@ -8,18 +8,25 @@
 import Hero
 import UIKit
 
+class PagingCollectionViewCell: UICollectionViewCell {}
+
 class ShoeGalleryCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var shoeImageView: UIImageView!
 }
 
-class ShoeInspectionViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+var galleryIndexPosition: Int = 0
 
-    @IBOutlet weak var primaryImageView: UIImageView!
+class ShoeInspectionViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    var data: Shoe? = nil
+    
     @IBOutlet weak var primaryImageBackdrop: UIView!
     @IBOutlet weak var shoeNameLabel: UILabel!
     @IBOutlet weak var shoePriceLabel: UILabel!
-    
     @IBOutlet weak var collectionView: UICollectionView!
+    
+    //paging control strip
+    @IBOutlet weak var pagingCollectionView: UICollectionView!
     
     @IBAction func returnButtonAction(_ sender: Any) {
         dismiss(animated: true, completion: nil)
@@ -28,31 +35,119 @@ class ShoeInspectionViewController: UIViewController, UICollectionViewDelegate, 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        primaryImageView.heroID = shoesArray[selectedPreviewIndex!].primaryImage
-        primaryImageView.image = UIImage(named: shoesArray[selectedPreviewIndex!].primaryImage)
+        data = shoesArray[selectedPreviewIndex!]
         
         primaryImageBackdrop.heroID = "backdropPreview-\(selectedPreviewIndex!)"
         primaryImageBackdrop.layer.cornerRadius = 50
         primaryImageBackdrop.layer.cornerCurve = .continuous
         primaryImageBackdrop.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
         
-        shoeNameLabel.heroID = "name-\(shoesArray[selectedPreviewIndex!].primaryImage)"
-        shoeNameLabel.text = shoesArray[selectedPreviewIndex!].name
-        shoePriceLabel.heroID = "price-\(shoesArray[selectedPreviewIndex!].primaryImage)"
-        shoePriceLabel.text = shoesArray[selectedPreviewIndex!].price
+        shoeNameLabel.heroID = "name-\(data!.primaryImage)"
+        shoeNameLabel.text = data!.name
+        shoePriceLabel.heroID = "price-\(data!.primaryImage)"
+        shoePriceLabel.text = data!.price
+        
+        collectionView.isPagingEnabled = true
+        collectionView.tag = 0
+        
+        pagingCollectionView.tag = 1
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        if collectionView.tag == 0 {
+            return data!.inspectImages.count + 1
+        } else if collectionView.tag == 1 {
+            return data!.inspectImages.count + 1
+        }
+        return 0
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ShoeGalleryCollectionViewCell", for: indexPath) as! ShoeGalleryCollectionViewCell
-        
-//        cell.shoeImageView.image = UIImage(named: <#T##String#>)
-        
-        return cell
+        if collectionView.tag == 0 {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ShoeGalleryCollectionViewCell", for: indexPath) as! ShoeGalleryCollectionViewCell
+            if indexPath.row == 0 {
+                cell.shoeImageView.heroID = data!.primaryImage
+                cell.shoeImageView.image = UIImage(named: data!.primaryImage)
+            } else {
+                cell.shoeImageView.image = UIImage(named: data!.inspectImages[indexPath.row - 1])
+            }
+            return cell
+        } else if collectionView.tag == 1 {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PagingCollectionViewCell", for: indexPath) as! PagingCollectionViewCell
+            
+            if indexPath.row == galleryIndexPosition {
+                cell.backgroundColor = .black
+            } else {
+                cell.backgroundColor = UIColor(white: 0.85, alpha: 1)
+            }
+            cell.layer.cornerRadius = cell.frame.height / 2
+            cell.layer.cornerCurve = .continuous
+            
+            return cell
+        }
+        return UICollectionViewCell()
     }
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        return
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        if collectionView.tag == 0 {
+            return CGSize(width: view.frame.width, height: 150)
+        } else if collectionView.tag == 1 {
+            if indexPath.row == galleryIndexPosition {
+                return CGSize(width: 22.5, height: 5)
+            } else {
+                return CGSize(width: 10, height: 5)
+            }
+        }
+        return CGSize(width: 50, height: 50)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        if collectionView.tag == 0 {
+            return 0
+        } else if collectionView.tag == 1 {
+            return 9
+        }
+        return 0
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        if collectionView.tag == 1 {
+            let totalWidth: CGFloat = (CGFloat(data!.inspectImages.count) * (10 + 9)) + 22.5
+            let insetValue: CGFloat = (view.frame.width - totalWidth) / 2
+
+            return UIEdgeInsets(top: 0, left: insetValue, bottom: 0, right: insetValue)
+        }
+        return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+//    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+//        <#code#>
 //    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        galleryIndexPosition = Int(ceil(scrollView.contentOffset.x / scrollView.frame.size.width))
+        pagingCollectionView.reloadData()
+    }
+}
+
+extension UICollectionView {
+    func scrollToNearestVisibleCollectionViewCell() {
+        self.decelerationRate = UIScrollView.DecelerationRate.fast
+        let visibleCenterPositionOfScrollView = Float(self.contentOffset.x + (self.bounds.size.width / 2))
+        var closestCellIndex = -1
+        var closestDistance: Float = .greatestFiniteMagnitude
+        for i in 0..<self.visibleCells.count {
+            let cell = self.visibleCells[i]
+            let cellWidth = cell.bounds.size.width
+            let cellCenter = Float(cell.frame.origin.x + cellWidth / 2)
+
+            // Now calculate closest cell
+            let distance: Float = fabsf(visibleCenterPositionOfScrollView - cellCenter)
+            if distance < closestDistance {
+                closestDistance = distance
+                closestCellIndex = self.indexPath(for: cell)!.row
+            }
+        }
+        if closestCellIndex != -1 {
+            self.scrollToItem(at: IndexPath(row: closestCellIndex, section: 0), at: .centeredHorizontally, animated: true)
+        }
+    }
 }
--- Apps/Nike Shoe App/Nike Shoe App/view controllers/shoe-data.swift
+++ Apps/Nike Shoe App/Nike Shoe App/view controllers/shoe-data.swift
@@ -23,25 +23,25 @@ struct ShoeSize {
 }
 
 var shoesArray: Array<Shoe> = [
-    Shoe(name: "Nike Air Zoom G.T. Run", price: "$175.00", primaryImage: "air-zoom-gt-run", inspectImages: [], sizes: [
+    Shoe(name: "Nike Air Zoom G.T. Run", price: "$175.00", primaryImage: "air-zoom-gt-run", inspectImages: ["air-zoom-gt-run-2", "air-zoom-gt-run-4", "air-zoom-gt-run-5", "air-zoom-gt-run-1"], sizes: [
         ShoeSize(size: 40, available: true), ShoeSize(size: 40.5, available: false), ShoeSize(size: 41, available: false), ShoeSize(size: 42, available: true), ShoeSize(size: 42.5, available: true), ShoeSize(size: 43, available: false), ShoeSize(size: 44, available: true), ShoeSize(size: 45, available: true), ShoeSize(size: 46, available: true), ShoeSize(size: 47.5, available: true), ShoeSize(size: 48, available: true), ShoeSize(size: 48.5, available: false), ShoeSize(size: 49, available: false)
     ], liked: false, description: "Recommended for players that look for cushion and impact protection."),
-    Shoe(name: "Nike Kyrie 4", price: "$136.00", primaryImage: "kyrie-4", inspectImages: [], sizes: [
+    Shoe(name: "Nike Kyrie 4", price: "$136.00", primaryImage: "kyrie-4", inspectImages: ["kyrie-4-1", "kyrie-4-2", "kyrie-4-3", "kyrie-4-4"], sizes: [
         ShoeSize(size: 41, available: false), ShoeSize(size: 42, available: true), ShoeSize(size: 42.5, available: true), ShoeSize(size: 43, available: true), ShoeSize(size: 44, available: true), ShoeSize(size: 45, available: true), ShoeSize(size: 46, available: false), ShoeSize(size: 47.5, available: false), ShoeSize(size: 48, available: true), ShoeSize(size: 48.5, available: true), ShoeSize(size: 49, available: true)
     ], liked: false, description: "Affordable, well-rounded basketball shoe."),
-    Shoe(name: "Nike Kyrie Low 2", price: "$154.00", primaryImage: "kyrie-low-2", inspectImages: [], sizes: [
+    Shoe(name: "Nike Kyrie Low 2", price: "$154.00", primaryImage: "kyrie-low-2", inspectImages: ["kyrie-low-2-1", "kyrie-low-2-2", "kyrie-low-2-3", "kyrie-low-2-4"], sizes: [
         ShoeSize(size: 42, available: true), ShoeSize(size: 42.5, available: false), ShoeSize(size: 43, available: true), ShoeSize(size: 44, available: true), ShoeSize(size: 45, available: true), ShoeSize(size: 46, available: true)
     ], liked: false, description: "Works best for narrow-footers looking for a good allrounder with responsive cushioning."),
-    Shoe(name: "Nike Mamba Instinct", price: "$125.00", primaryImage: "mamba-instinct", inspectImages: [], sizes: [
+    Shoe(name: "Nike Mamba Instinct", price: "$125.00", primaryImage: "mamba-instinct", inspectImages: ["mamba-instinct-1", "mamba-instinct-2", "mamba-instinct-3"], sizes: [
         ShoeSize(size: 40, available: true), ShoeSize(size: 40.5, available: true), ShoeSize(size: 41, available: true), ShoeSize(size: 42, available: true), ShoeSize(size: 42.5, available: true), ShoeSize(size: 43, available: false), ShoeSize(size: 44, available: true), ShoeSize(size: 45, available: false), ShoeSize(size: 46, available: true)
     ], liked: false, description: "Meant for quick guards who want a light shoe."),
-    Shoe(name: "Nike Zoom Freak 1", price: "$83.00", primaryImage: "zoom-freak-1", inspectImages: [], sizes: [
+    Shoe(name: "Nike Zoom Freak 1", price: "$83.00", primaryImage: "zoom-freak-1", inspectImages: ["zoom-freak-1-1", "zoom-freak-1-2", "zoom-freak-1-3", "zoom-freak-1-4"], sizes: [
         ShoeSize(size: 40, available: true), ShoeSize(size: 40.5, available: true), ShoeSize(size: 41, available: true), ShoeSize(size: 42, available: false), ShoeSize(size: 42.5, available: false)
     ], liked: false, description: "Very low-to-the-ground feeling with very firm forefoot-cushioning."),
-    Shoe(name: "Nike PG1", price: "$160.00", primaryImage: "pg-1", inspectImages: [], sizes: [
+    Shoe(name: "Nike PG1", price: "$160.00", primaryImage: "pg-1", inspectImages: ["pg-1-1", "pg-1-2", "pg-1-3", "pg-1-4"], sizes: [
         ShoeSize(size: 42.5, available: true), ShoeSize(size: 43, available: false), ShoeSize(size: 44, available: true), ShoeSize(size: 45.5, available: true), ShoeSize(size: 46, available: true), ShoeSize(size: 47, available: false), ShoeSize(size: 48, available: false)
     ], liked: false, description: "Works best for guards with regular or narrow feet."),
-    Shoe(name: "Nike Lebron 15 Low", price: "$153.00", primaryImage: "lebron-15-low", inspectImages: [], sizes: [
+    Shoe(name: "Nike Lebron 15 Low", price: "$153.00", primaryImage: "lebron-15-low", inspectImages: ["lebron-15-low-1", "lebron-15-low-2", "lebron-15-low-3", "lebron-15-low-4"], sizes: [
         ShoeSize(size: 46, available: true), ShoeSize(size: 47.5, available: true), ShoeSize(size: 48, available: true), ShoeSize(size: 48.5, available: false), ShoeSize(size: 49, available: false), ShoeSize(size: 49.5, available: true), ShoeSize(size: 50, available: false), ShoeSize(size: 51, available: false)
     ], liked: false, description: "Great shoe for explosive and heavier players."),
 ]
